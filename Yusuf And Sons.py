d17182c432678b6bba21131e66844e77ef561e86
Ochuba <ekenedilichukwu.ochuba@binghamuni.edu.ng>
2023-11-11 10:39:11 +0100

    Final Submission

--- Yusuf And Sons.py
+++ Yusuf And Sons.py
@@ -1,3 +1,5 @@
+print('Welcome to Yusuf and Sons Company!')
+print('To Calculate your Simple and Compound Interest:')
 p = float(input("Enter Initial Principal: "))
 
 r = float(input("Enter Interest Rate: "))
@@ -9,6 +11,7 @@ n = float(input("Enter Number of Times: "))
 Simple_Interest = str(p * (1 + r * t))
 Compound_Interest = str(p * (1 + r/n) ** n * t)
 
-print("YUSUF AND SONS")
+print("YUSUF AND SONS COMPANY")
 print("SIMPLE INTEREST = " + Simple_Interest)
 print("COMPOUND INTEREST = " + Compound_Interest)
+print('Thank You!')
